9dd992f7dcc95cce29c76fbfd2cd949f127230d6
mihailo.plavsic@aspiretechnology.com <mihailo.plavsic@aspiretechnology.com>
2024-06-02 22:29:32 +0200

    Updated LSP conf

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -15,9 +15,20 @@ require('mason-lspconfig').setup({
 --require('lspconfig').rust_analyzer.setup({})
 
 lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    lsp_zero.default_keymaps({buffer = bufnr})
+    local opts = {buffer = bufnr, remap = false}
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 local cmp = require('cmp')
@@ -28,21 +39,6 @@ local cmp_mappings = lsp_zero.defaults.cmp_mappings({
   ['<C-y>'] = cmp.mapping.confirm({ select = true }),
   ["<C-Space>"] = cmp.mapping.complete(),
 })
-
---lsp_zero.on_attach(function(client, bufnr)
- -- local opts = {buffer = bufnr, remap = false}
-
---  vim.keymap.set("n", "gd", function() vim.lsp_zero.buf.definition() end, opts)
---  vim.keymap.set("n", "K", function() vim.lsp_zero.buf.hover() end, opts)
---  vim.keymap.set("n", "<leader>vws", function() vim.lsp_zero.buf.workspace_symbol() end, opts)
---  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
---  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
---  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
---  vim.keymap.set("n", "<leader>vca", function() vim.lsp_zero.buf.code_action() end, opts)
---  vim.keymap.set("n", "<leader>vrr", function() vim.lsp_zero.buf.references() end, opts)
---  vim.keymap.set("n", "<leader>vrn", function() vim.lsp_zero.buf.rename() end, opts)
---  vim.keymap.set("i", "<C-h>", function() vim.lsp_zero.buf.signature_help() end, opts)
---end)
 
 lsp_zero.setup()
 
