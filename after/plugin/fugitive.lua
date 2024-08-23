51e201bfac1bcc5595f00434bf371ab87df81dbc
Mihailo Plavsic <mihailoplavsic34@gmail.com>
2024-08-23 19:15:01 +0200

    small changes

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,2 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+vim.keymap.set("n", "<leader>ga", function() vim.cmd('Git add .') end)
--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,20 +1,30 @@
 local lsp_zero = require('lsp-zero')
 
-lsp_zero.setup_servers({'pyright', 'rust_analyzer', 'yamlls', 'tsserver', 'dockerls', 'lua_ls', 'bashls' })
+lsp_zero.setup_servers({ 'pyright', 'rust_analyzer', 'yamlls', 'tsserver', 'dockerls', 'lua_ls', 'bashls'})
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'pyright', 'rust_analyzer', 'yamlls', 'tsserver', 'dockerls', 'lua_ls', 'bashls' },
-  handlers = {
-    lsp_zero.default_setup,
-  },
+    ensure_installed = { 'pyright', 'rust_analyzer', 'yamlls', 'tsserver', 'dockerls', 'lua_ls', 'bashls' },
+    handlers = {
+        lsp_zero.default_setup,
+    },
+})
+require("lspconfig").terraformls.setup({
+    init_options = {
+        experimentalFeatures = {
+            prefillRequiredFields = true,
+        },
+        -- terraform = {
+        --     path = "/usr/bin/terraform-ls"
+        -- }
+    }
 })
 
 lsp_zero.on_attach(function(client, bufnr)
     -- see :help lsp-zero-keybindings
     -- to learn the available actions
-    lsp_zero.default_keymaps({buffer = bufnr})
-    local opts = {buffer = bufnr, remap = false}
+    lsp_zero.default_keymaps({ buffer = bufnr })
+    local opts = { buffer = bufnr, remap = false }
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
@@ -28,12 +38,12 @@ lsp_zero.on_attach(function(client, bufnr)
 end)
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp_zero.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
 })
 
 lsp_zero.setup()
