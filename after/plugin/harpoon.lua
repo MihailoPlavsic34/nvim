3d102fdecea438b1fd053a50c84e433aa87efe34
Mihailo Plavsic <mihailoplavsic34@gmail.com>
2024-08-25 22:38:26 +0200

    harpoon keybindings changed

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -7,10 +7,10 @@ vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
 
-vim.keymap.set("n", "<leader>1", function() terminal.gotoTerminal(1) end)
-vim.keymap.set("n", "<leader>2", function() terminal.gotoTerminal(2) end)
-vim.keymap.set("n", "<leader>3", function() terminal.gotoTerminal(3) end)
-vim.keymap.set("n", "<leader>4", function() terminal.gotoTerminal(4) end)
+vim.keymap.set("n", "<leader>t", function() terminal.gotoTerminal(1) end)
+vim.keymap.set("n", "<leader>h", function() terminal.gotoTerminal(2) end)
+vim.keymap.set("n", "<leader>n", function() terminal.gotoTerminal(3) end)
+vim.keymap.set("n", "<leader>s", function() terminal.gotoTerminal(4) end)
 
 vim.keymap.set("n", "<C-t>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<C-h>", function() ui.nav_file(2) end)
